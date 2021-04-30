48a4a563afcdbcb759a1af25466d15bdd3dfaa2f
Milo Weinberg <iapetus011@gmail.com>
2021-04-30 10:11:25 -0400

    put shard id in status for debugging

--- src/cogs/core/loops.py
+++ src/cogs/core/loops.py
@@ -24,7 +24,7 @@ class Loops(commands.Cog):
     @tasks.loop(minutes=45)
     async def change_status(self):
         await self.bot.wait_until_ready()
-        await self.bot.change_presence(activity=discord.Game(name=random.choice(self.d.playing_list)))
+        await self.bot.change_presence(activity=discord.Game(name=f"[{self.bot.shard_id}] {random.choice(self.d.playing_list)}"))
 
     @tasks.loop(hours=24)
     async def update_fishing_prices(self):
